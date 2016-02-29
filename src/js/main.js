03c4622eb630af08e887a4cab28ecc89e66351d2
Drew Sancho <drew_sancho@yahoo.com>
2016-02-29 10:53:43 -0500

    second crime

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,25 @@
+// (function () {
+//     var time = document.getElementsByClassName('time-to-read');
+//     var x = document.querySelector('p');
+//     function timeToRead (x) {
+//         var minutes = Math.ceil(x.textContent.split(' ').length / 230);
+//         if (minutes === 0) {
+//             minutes = 1;
+//         }
+//         return minutes;
+//     };
+//     var minutes = timeToRead(x);
+//     time[0].innerHTML = 'This article will take ' + minutes + ' minutes to read.';
+// })();
+
 (function () {
     var time = document.getElementsByClassName('time-to-read');
-    var x = document.querySelector('p');
-    function timeToRead (x) {
-        var minutes = Math.floor(x.textContent.split(' ').length / 230);
-        if (minutes === 0) {
-            minutes = 1;
-        }
-        return minutes;
-    };
-    var minutes = timeToRead(x);
+    var x = document.querySelectorAll('p');
+    var y = Array.prototype.slice.call(x);
+    // var minutes = wordcount() / 230;
+    var wordCount = y.reduce(function (a, b) {
+        return a + b.textContent.split(' ').length;
+    }, 0);
+    var minutes = Math.ceil(wordCount / 230);
     time[0].innerHTML = 'This article will take ' + minutes + ' minutes to read.';
 })();
